5fd89a817463e90e4ed2d3646d7e1c15ad2a9a48
Joey Brown <joeyrb93@gmail.com>
2018-06-21 08:54:38 -0600

    new folder + housekeeping

--- plotLogData.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import parseLogFile as PLF
-# plt.plot([1, 2, 3, 4])
-variable = 'gxmn'
-params = PLF.getParams(variable)
-plt.plot(params)
-plt.ylabel(variable)
-plt.xlabel('health check interval')
-plt.show()
